677db7fd7c84322c0aa3728ee1d759ab206dff2d
Telisa Lombard <telisa.lombard@theodo.com>
2025-11-20 17:25:20 +0200

    feat(P004): added the navigation package; made cards clickable and on click navigate to the Pokemon Details screen

--- App.tsx
+++ App.tsx
@@ -5,52 +5,18 @@ import { use, useEffect, useState } from 'react';
 import { IndexedPokemon, NamedAPIResourceList } from './services/pokeAPI.type';
 import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
 import PokemonCard from './components/PokemonCard';
+import { NavigationContainer } from '@react-navigation/native';
+import AppNavigator from './navigation/AppNavigator';
 
 export default function App() {
-  const [items, setItems] = useState<IndexedPokemon[]>([]);
   
-  const loadPokemons = async () => {
-    try {
-      const pokemons = await listPokemons();
-      const pokemonsWithIds = injectId(pokemons.results);
-      setItems(pokemonsWithIds);
-      console.log("Successfully loaded pokemons");
-    } catch (error) {
-      console.error("Error loading pokemons:", error);
-    }
-  }
-  
-  useEffect(() => {
-    loadPokemons();
-  }, []);
 
   return (
-    <SafeAreaProvider>
-      <SafeAreaView style={styles.container}>
-        <FlatList
-          data={items}
-          renderItem={({ item }: { item: IndexedPokemon }) => (
-            <PokemonCard pokemon={item} />
-          )}
-          keyExtractor={(item) => item.id.toString()}
-          contentContainerStyle={{ paddingHorizontal: 10 }}
-        />
-        <StatusBar style="auto" />
-      </SafeAreaView>
-    </SafeAreaProvider>
+    <NavigationContainer>
+      <AppNavigator />
+    </NavigationContainer>
+    
   );
 }
 
-const Item = ({ title, id }: { title: string; id: number }) => (
-  <View>
-    <Text>{id} {title}</Text>
-  </View>
-);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    marginInline: 20,
-  },
-});
---
+++ screens/Home.tsx
@@ -0,0 +1,69 @@
+import { View, Text, Pressable } from "react-native";
+import { IndexedPokemon } from "../services/pokeAPI.type";
+import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
+import { FlatList, StyleSheet } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { use, useEffect, useState } from 'react';
+import PokemonCard from "../components/PokemonCard";
+import { injectId, listPokemons } from "../services/pokeAPI";
+import { useNavigation } from '@react-navigation/native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { RootStackParamList } from '../navigation/AppNavigator'; 
+
+export default function HomeScreen() {
+  const [items, setItems] = useState<IndexedPokemon[]>([]);
+  
+  const loadPokemons = async () => {
+    try {
+      const pokemons = await listPokemons();
+      const pokemonsWithIds = injectId(pokemons.results);
+      setItems(pokemonsWithIds);
+      console.log("Successfully loaded pokemons");
+    } catch (error) {
+      console.error("Error loading pokemons:", error);
+    }
+  }
+  
+  useEffect(() => {
+    loadPokemons();
+  }, []);
+
+  const navigation = useNavigation<NativeStackNavigationProp<RootStackParamList>>();
+
+  return (
+    <SafeAreaProvider>
+        <SafeAreaView style={styles.container}>
+            <FlatList
+            data={items}
+            renderItem={({ item }: { item: IndexedPokemon }) => (
+                <Pressable
+                  onPress={() => {
+                    console.log(`Pressed on ${item.name}`);
+                    navigation.navigate('PokemonDetails', { pokemonId: item.id });
+                  }}
+                >
+                  <PokemonCard pokemon={item} />
+                </Pressable>
+            )}
+            keyExtractor={(item) => item.id.toString()}
+            contentContainerStyle={{ paddingHorizontal: 10 }}
+            />
+            <StatusBar style="auto" />
+        </SafeAreaView>
+    </SafeAreaProvider>
+  );
+}
+
+const Item = ({ title, id }: { title: string; id: number }) => (
+  <View>
+    <Text>{id} {title}</Text>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    marginInline: 20,
+  },
+});
